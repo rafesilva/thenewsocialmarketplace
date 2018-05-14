6040642559fb69acf3db025978ba7db7b9d45c67
Rafael FS <rafesilva@gmail.com>
2018-05-15 01:33:02 +1000

    first commit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -23,10 +23,6 @@ class TransactionsController < ApplicationController
             :account => ENV['CONNECTED_STRIPE_ACCOUNT_ID']
     }
 
-      purchase = Purchase.create( email: params[:stripeEmail], card: params[:stripeToken], amount: params[:amount], description: charge.description, currency: charge.currency, customer_id: customer.id, product_id:1 )
-    
-      redirect_to purchase
-
         
       rescue Stripe::CardError => e
         flash[:error] = e.message
