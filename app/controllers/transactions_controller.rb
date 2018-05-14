361d86b03e42738900d7da88ec03589cab801b5e
Rafael FS <rafesilva@gmail.com>
2018-05-15 01:35:06 +1000

    first commit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,31 +2,31 @@ class TransactionsController < ApplicationController
     include CurrentCart
     before_action :set_cart
     
-    def create
+    # def create
 
-      Stripe.api_key = Rails.configuration.stripe[:stripe_secret_key]
-        # Amount in cents
-        @amount = (@cart.total_price)*10
+    #   Stripe.api_key = Rails.configuration.stripe[:stripe_secret_key]
+    #     # Amount in cents
+    #     @amount = (@cart.total_price)*10
       
-        customer = Stripe::Customer.create(
-          :email => params[:stripeEmail],
-          :source  => params[:stripeToken]
-        )
+    #     customer = Stripe::Customer.create(
+    #       :email => params[:stripeEmail],
+    #       :source  => params[:stripeToken]
+    #     )
       
-        charge = Stripe::Charge.create(
-          :customer    => customer.id,
-          :amount      => params[:amount],
-          :description => 'Rails Stripe customer',
-          :currency    => 'usd',
-          :destination => {
-            :amount => 10,
-            :account => ENV['CONNECTED_STRIPE_ACCOUNT_ID']
-    }
+    #     charge = Stripe::Charge.create(
+    #       :customer    => customer.id,
+    #       :amount      => params[:amount],
+    #       :description => 'Rails Stripe customer',
+    #       :currency    => 'usd',
+    #       :destination => {
+    #         :amount => 10,
+    #         :account => ENV['CONNECTED_STRIPE_ACCOUNT_ID']
+    # }
 
         
-      rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to streaming_index_path, notice: "Error"
-      end
-    end
+    #   rescue Stripe::CardError => e
+    #     flash[:error] = e.message
+    #     redirect_to streaming_index_path, notice: "Error"
+    #   end
+    # end
   
